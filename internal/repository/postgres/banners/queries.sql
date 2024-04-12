e0c2387e5a08c9a833cf6d35cb3afe96ca62443c
vinilco <vinilco2@gmail.com>
2024-04-12 23:56:03 +0300

    refactor ListBanners

--- internal/repository/postgres/banners/queries.sql
+++ internal/repository/postgres/banners/queries.sql
@@ -6,7 +6,7 @@ FROM banners
 WHERE banners_tag.tag_id = $1
   AND banners.feature_id = $2
 ORDER BY banners_info.updated_at DESC
-    LIMIT 1;
+LIMIT 1;
 
 -- name: CheckActiveUserBanner :one
 SELECT banners.is_active
@@ -18,28 +18,22 @@ WHERE banners_tag.tag_id = $1
 -- name: ListBanners :many
 SELECT banners.id,
        banners.feature_id,
-       banners_info.contents,
+       bi.contents,
        banners.is_active,
        banners.created_at,
-       banners_info.updated_at,
-       tags_arr.tags,
-       RANK() OVER (
-           PARTITION BY banners_tag.tag_id, banners.feature_id
-           ORDER BY banners_info.updated_at DESC
-           ) rn
+       bi.updated_at,
+       array_agg(banners_tag.tag_id) as tags
 FROM banners
          JOIN banners_tag ON banners.id = banners_tag.banner_id
-         JOIN banners_info ON banners_tab.banner_id = banners_info.banner_id
-         JOIN (SELECT banners.id, array_agg(banners_tag.tag_id) as tags
-               FROM banners
-                        JOIN banners_tag ON banner.id = banners_tag.banner_id
-               GROUP BY banners.id) tags_arr
-              ON banners.id = tags_arr.id
+         JOIN (select banner_id, contents, updated_at
+               from banners_info
+               order by updated_at desc
+               limit 1) as bi ON banners_tag.banner_id = bi.banner_id
 WHERE banners_tag.tag_id = $1
    OR $1 IS NULL AND banners.feature_id = $2
-   OR $2 IS NULL AND rn = 1
-    LIMIT $3
-OFFSET $4;
+   OR $2 IS NULL AND tag_id = $1
+group by 1, 2, 3, 4, 5, 6
+LIMIT $3 OFFSET $4;
 
 -- name: ListBannerVersions :many
 SELECT banners.id,
@@ -51,9 +45,8 @@ SELECT banners.id,
 FROM banners
          JOIN banners_tag ON banners.id = banners_tag.banner_id
          JOIN banners_info ON banners_tag.banner_id = banners_info.banner_id
-WHERE banners_tag.tag_id = $1 AND banners.feature_id = $2 AND banner.id >= $4
-order by banners.id
-LIMIT $3;
+WHERE banners_tag.tag_id = $1 AND banners.feature_id = $2
+LIMIT $3 OFFSET $4;
 
 -- name: CheckBannerId :one
 SELECT EXISTS(SELECT id FROM banners WHERE id = $1);
@@ -89,7 +82,7 @@ VALUES ($1, UNNEST($2::INT[]));
 -- name: CreateBanner :one
 INSERT INTO banners (feature_id, is_active, created_at)
 VALUES ($1, $2, NOW())
-    RETURNING id;
+RETURNING id;
 
 -- name: CreateBannerInfo :exec
 INSERT INTO banners_info (banner_id, updated_at, contents)
